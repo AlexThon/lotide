99442fd1839bb3fc669a969f8e2406131c327f8a
Alex Thon <ghaiquol@gmail.com>
2021-10-27 04:04:11 +0000

    Add eqArrays.js file

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+
+const eqArrays = (arrayA, arrayB) => {
+    if (arrayA.length !== arrayB.length) {
+      return false;
+    }
+    for (const item of arrayB) {
+      if (!arrayA.includes(item)) {
+        return false;
+      }
+    }
+    return true;
+  };
+  
+
+const assertEqual = (actual, expected) => {
+    if (actual === expected) {
+        console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+        console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+    }
+} 
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
