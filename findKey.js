6458533ceae3462bea159c416b0e5359ed485f11
Alex Thon <ghaiquol@gmail.com>
2021-10-29 06:03:27 +0000

    Add findKey.js file

---
+++ findKey.js
@@ -0,0 +1,28 @@
+
+// takes in an object and a callback.
+// return the first key for which the callback returns a truthy value
+
+const findKey = (ourObject, callback) => {
+  for (const key in ourObject) {
+    if (callback(ourObject[key])) {
+      return key;
+    }
+  }
+  
+};
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅️ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(` 🛑️ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }}, x => x.stars === 2), "noma"); // => "noma"
