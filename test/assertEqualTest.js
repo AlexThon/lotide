de0f6d72ef4cb29c50b2513637c1f9b252031b4a
Alex Thon <ghaiquol@gmail.com>
2021-11-02 01:19:31 +0000

    Add mocha test cases for assertArraysEqual

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,11 +1,9 @@
 const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-//assertEqual("Lighthouse Labs", "Lighthouse Labs");
 describe("assertEqual()", () => {
   it('Should return true for assertEqual(1,1)', () => {
     assert.strictEqual(assertEqual(1,1));
   });
 });
 
-assertEqual(1,1);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,11 @@
+
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual.js');
-const tail = require('../tail.js');
-assertArraysEqual(tail([1,3,4,5]), [3,4,5]);
+
+describe("assertArraysEqual()", () => {
+  it('Should return not equal (!==) for [1,3,4,5] and [3,4,5]', () => {
+    assert.deepEqual(assertArraysEqual([1,3,4,5],[3,4,5]));
+  });
+});
+
 
